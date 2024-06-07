5f63089a4ad446c2ab9a8f5e9a2d2e6b34138078
Muskanjadhav <muskanjadhav109@gmail.com>
2024-06-07 21:15:57 +0530

    add files

---
+++ nbproject/src/ASimulatorSystem/calendar/JDateChooser.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ASimulatorSystem.calendar;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author abhis
+ */
+public class JDateChooser {
+
+    public void setForeground(Color color) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setBounds(int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Object getDateEditor() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+       
+}
